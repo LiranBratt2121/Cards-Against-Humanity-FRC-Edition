85613469aa1a221a9f4a03127d57c6b47af6ceff
LiranBratt2121 <brattlirannin@gmail.com>
2023-10-02 15:56:30 +0300

    Add Room Creation screen

---
+++ screens/CreateRoomScreen.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import { useState } from 'react';
+
+import CodeCopyButton from "../components/CodeCopyButton";
+
+const CreateRoomScreen = () => {
+  const [pressed, setPressed] = useState(false)
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Create Room Screen</Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => setPressed(!pressed)}>
+        <Text style={styles.buttonFont}>
+          Generate Room Code
+        </Text>
+      </TouchableOpacity>
+
+      <View style={styles.codeCopyBoxContainer}>
+        {pressed && (
+          <CodeCopyButton
+            code={"Example Code"}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default CreateRoomScreen;
