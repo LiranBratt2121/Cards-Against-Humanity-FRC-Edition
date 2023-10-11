4519b7c7b8183800a99267be49dd7102a8d8e175
LiranBratt2121 <brattlirannin@gmail.com>
2023-10-11 15:10:21 +0300

    Add Room Password gen

---
+++ screens/utils.js
@@ -0,0 +1,12 @@
+generateRoomPassword = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let password = '';
+
+    for (let i = 0; i < 6; i++){
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return password;
+} 
+
+export default generateRoomPassword
